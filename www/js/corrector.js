dd9ee57e5e6f6dc6863aed006049acd242fb45ce
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2017-09-18 23:16:41 +0300

    Implement AJAX save propositions

--- www/js/corrector.js
+++ www/js/corrector.js
@@ -16,17 +16,43 @@ $(document).ready(function () {
         dataType : "json",
         success : function (res) {
             $('#correctorForm').append('<h2>TITLE</h2><div class="form-group">\n' +
-                '    <label for="title">' + res.title + '</label>\n' +
+                '    <label for="pTitle">' + res.title + '</label>\n' +
                 '    <input type="text" id="title" class="form-control input-lg" name="title" value="' + res.title + '"/>' +
-                '  </div><button class="btn btn-lg btn-primary btn-block signup-btn" value="p' + i +'" type="submit">\n' +
+                '  </div><button class="btn btn-lg btn-primary btn-block signup-btn" value="pTitle" type="button">\n' +
                 '  Send</button><hr/><h2>Paragraphs</h2>');
             for(var i = 0; i < res.paragraphs.length; i++) {
                 $('#correctorForm').append('<div class="form-group">\n' +
                     '    <label for="p' + i + '">' + res.paragraphs[i] + '</label>\n' +
                     '    <textarea class="form-control input-lg" id="p' + i + '">' + res.paragraphs[i] + '</textarea>\n' +
-                    '  </div><button class="btn btn-lg btn-primary btn-block signup-btn" value="p' + i +'" type="submit">\n' +
+                    '  </div><button class="btn btn-lg btn-primary btn-block signup-btn" value="p' + i +'" id="corrrectionsButton" type="button">\n' +
                     '  Send</button><hr/>');
             }
+
+            $('button').click(function () {
+                var code = $(this).val();
+                var oText = $('label[for=' + code + ']').text();
+                var uText = code == 'pTitle' ? $('#title').val() : $('#' + code).val();
+
+                sendUpdate(oText, uText);
+            });
         }
     });
+
+
+    function sendUpdate(oText, uText) {
+        $.ajax({
+            url : '/article',
+            method : "post",
+            dataType : "json",
+            data : JSON.stringify({
+                articleURL : $.urlParam('articleURL'),
+                originalText : oText,
+                usersText : uText
+            }),
+            processData: false,
+            success : function () {
+                alert("Your proposition sent");
+            }
+        });
+    }
 });
