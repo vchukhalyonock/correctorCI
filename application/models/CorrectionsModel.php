ab49799224afaba85d4ed4e570097e8f55694ebf
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2017-09-18 23:52:20 +0300

    Add CorrectionsModel methods setApproved and delete

--- application/models/CorrectionsModel.php
+++ application/models/CorrectionsModel.php
@@ -26,4 +26,37 @@ class CorrectionsModel extends CI_Model {
 
         return $this->db->insert_id();
     }
+
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function setApproved($id) {
+        $this->db->update(
+            $this->_table,
+            [
+                'isApproved' => 1
+            ],
+            [
+                'id' => intval($id)
+            ]
+        );
+
+        return true;
+    }
+
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function delete($id) {
+        $this->db->delete(
+            $this->_table,
+            ['id' => intval($id)]
+        );
+
+        return true;
+    }
 }
